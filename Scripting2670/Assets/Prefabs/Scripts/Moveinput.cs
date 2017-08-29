bd6cba502bb43e35a00313359b407d8e0f7334e7
Unknown <caboosereynolds@gmail.com>
2017-08-28 22:01:01 -0600

    test scripts for jump

--- Scripting2670/Assets/Prefabs/Scripts/Moveinput.cs
+++ Scripting2670/Assets/Prefabs/Scripts/Moveinput.cs
@@ -6,12 +6,22 @@ using System;
 public class Moveinput : MonoBehaviour {
 
 public static Action<float> KeyAction;
+    public float Speed;  //allows us to be able to change speed in Unity
+public Vector2 jumpHeight;
 
 void Update () {
 	if (KeyAction != null)
 	{
 		KeyAction(Input.GetAxis("Horizontal"));
 	}
+			{
+    transform.Translate(Speed * Time.deltaTime, 0f, 0f);  //makes player run
+
+    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  //makes player jump
+    {
+        GetComponent<Rigidbody>().AddForce(jumpHeight, ForceMode.Impulse);
+	}
+		}
 	}
 }
 /*private void Start ()
