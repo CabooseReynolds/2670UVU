b9cf24728e2086aad3fa669895dc2123f192a129
Unknown <caboosereynolds@gmail.com>
2017-09-01 09:14:34 -0600

    Bear Prototype Unity Project

---
+++ Bear Prototypes/Assets/Scripts/JumpChar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class JumpChar : MonoBehaviour {
+
+public static Action<float> KeyAction;
+public static Action<bool> JumpAction;
+
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController cc;
+
+	void Start () {
+	cc = GetComponent<CharacterController>();
+    Moveinput.JumpAction = Jump;
+
+	}
+
+    private void Jump(bool obj){
+            if (obj){
+            if (isGrounded()){
+Debug.Log(isGrounded());
+       //      moveDirection = new Vector3(0, jumpSpeed, 0);
+            moveDirection.y = jumpSpeed;}
+            }
+        moveDirection.y -= gravity * Time.deltaTime;
+        cc.Move(moveDirection * Time.deltaTime);
+            
+        }
+        bool isGrounded(){
+            return Physics.CheckSphere(new Vector3(0, -1.3f, 0), .1f);
+    
+}
+
+    
+}
