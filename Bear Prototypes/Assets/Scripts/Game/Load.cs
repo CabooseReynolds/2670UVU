fac6dd8578e715c607f5c9ff9f9e41cb88206b54
Unknown <caboosereynolds@gmail.com>
2017-09-10 23:05:37 -0600

    organizing items

---
+++ Bear Prototypes/Assets/Scripts/Game/Load.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+public class Load : MonoBehaviour {
+
+	public string sceneName;
+	public static Action EndButton;
+
+	void Start ()
+	{
+		Load.EndButton += DisableButton;
+	}
+
+	public void LoadLevel()
+	{
+		EndButton();
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+	}
+
+	public void DisableButton()
+	{
+		GetComponent<Button>().interactable = false;
+	}
+
+
+}
---
+++ Bear Prototypes/Assets/Scripts/Patrol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+public float moveSpeed;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (moveSpeed, 0, 0) * Time.deltaTime);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.gameObject.tag == "patrolarea")
+		{
+			moveSpeed *= -1;
+		}
+	}
+}
