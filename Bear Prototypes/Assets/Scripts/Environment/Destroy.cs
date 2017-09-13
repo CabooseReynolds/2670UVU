b2620981d1093a996dadbdcde9ff7b330f4b135a
Unknown <caboosereynolds@gmail.com>
2017-09-13 00:38:13 -0600

    prototype 2 done

---
+++ Bear Prototypes/Assets/Scripts/Environment/Destroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+public GameObject door;
+     // Use this for initialization
+     void OnTriggerEnter(Collider other)
+     { if (other.CompareTag ("door")) {
+             if (Input.GetKey (KeyCode.R)) {
+                         Destroy(other.gameObject);
+ 
+ }
+         }
+	 }
+}
