f197b1813674379f2ba66115634eb179071b896a
Unknown <caboosereynolds@gmail.com>
2017-09-15 13:51:40 -0600

    test

---
+++ Scripting2670/Assets/Scripts/NewScripts/Moveinput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Moveinput : MonoBehaviour {
+public float runTime = 0.01f;
+public static Action<float> KeyAction;
+//public static Action JumpAction;
+public static Action<bool> JumpAction;
+public bool canPlay = true; 
+ 
+
+void Start ()
+{
+	StartCoroutine(RunInput());
+}
+ IEnumerator RunInput (){
+	 while (canPlay)
+	 {
+	 	if(KeyAction != null)
+	 	{
+		 KeyAction(Input.GetAxis("Horizontal"));
+	 	}
+
+		 if(JumpAction != null)
+	 	{
+		 JumpAction(Input.GetButton("Jump"));
+		 }
+		yield return new WaitForSeconds(runTime);
+	 }
+ }
+}
