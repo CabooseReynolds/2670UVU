bc4b6862b781305e34e3a5abca75ccf87b32a165
Unknown <caboosereynolds@gmail.com>
2017-09-18 10:47:15 -0600

    Auto stash before rebase of "origin/master"
    Updates to boywalk cycle

---
+++ Bear Prototypes/Assets/Scripts/PlayerScripts/GrabAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class GrabAction : MonoBehaviour {
+
+GameObject Player;
+	// Use this for initialization
+	void Grabber ()
+	{
+		GetComponent<Rigidbody>().useGravity = false;
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+		transform.parent = Player.transform;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.tag == "Player")
+		{
+			Moveinput.grabby += Grabb
+		}	
+	}
+}
