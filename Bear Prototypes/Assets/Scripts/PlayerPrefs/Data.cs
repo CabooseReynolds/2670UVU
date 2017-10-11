a0f6d27511c1c4c26cc87a4e00d5eb62556cdf63
Unknown <caboosereynolds@gmail.com>
2017-10-11 15:27:05 -0600

    stuff for checkpoints

---
+++ Bear Prototypes/Assets/Scripts/PlayerPrefs/Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Data {
+	public int totalScore;
+	public float health;
+	public string playerName;
+	public Vector3 checkpoint;
+}
---
+++ Bear Prototypes/Assets/Scripts/PlayerPrefs/GetGameData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GetGameData : MonoBehaviour {
+
+	public Data data;
+	void Start ()
+	{
+		data = JsonUtility.FromJson<Data>(PlayerPrefs.GetString("GameData"));
+		transform.localPosition = data.checkpoint;
+	}
+	// public static SetGameData CreateFromJson(string jsonString)
+	// {
+	// 	return JsonUtility.FromJson<SetGameData>(jsonString);
+	// }
+	}
