296256acbacee4e2845e4b2d209f1bcc1e7b7e59
Unknown <caboosereynolds@gmail.com>
2017-10-11 00:46:50 -0600

    Quick save cause i don't want to lose everything again.

---
+++ Bear Prototypes/Assets/Scripts/Enemy/EnemyWeapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour {
+
+    public GameObject obj;
+    public float startDelay;
+    public float speed;
+	public float deathDelay;
+    public Vector3 endPoint;
+    public bool doorClosed = true;
+//    public Vector3 endPoint;
+void Start()
+			{
+			StartCoroutine(Shot());
+            }
+IEnumerator Shot ()
+{   
+    yield return new WaitForSeconds(startDelay);
+		while(obj.transform.position != endPoint){
+			obj.transform.position = Vector3.MoveTowards(obj.transform.position, endPoint, speed * Time.deltaTime);
+			Destroy(obj, deathDelay);
+			yield return null;
+		}
+    }
+}
+
---
+++ Bear Prototypes/Assets/Scripts/Enemy/SpawnIn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnIn : MonoBehaviour {
+
+	public GameObject spawnins;
+	public float spawnTime;
+	public bool runOnStart;
+
+	// Use this for initialization
+	void Start () {
+		if(runOnStart)
+		{
+			StartCoroutine(Spawn());
+		}
+	}
+
+	IEnumerator Spawn()
+	{
+		while(true)
+		{
+			Instantiate(spawnins, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(spawnTime);
+		}
+	}
+}
