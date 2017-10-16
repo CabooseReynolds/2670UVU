ded34c00f3a417cdd8bdd3e731ecfa9923885a29
Unknown <caboosereynolds@gmail.com>
2017-10-15 22:03:02 -0600

    Proto12 Build created
    Still laggy AF!

--- Bear Prototypes/Assets/Scripts/Old Scripts/Hidden2.cs
+++ Bear Prototypes/Assets/Scripts/Old Scripts/Hidden2.cs
@@ -2,25 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hidden : MonoBehaviour {
+public class Hidden2 : MonoBehaviour {
 
 // public GameObject[] Object;
 public GameObject Object1;
-public GameObject Object2;
-public GameObject Object3;
-public GameObject Object4;
-public GameObject Object5;
-public GameObject Object6;
-public GameObject Object7;
 int Objects = 0;
 private void OnTriggerEnter() {
     Object1.SetActive(false);
-    Object2.SetActive(false);
-    Object3.SetActive(false);
-    Object4.SetActive(false);
-    Object5.SetActive(false);
-    Object6.SetActive(false);
-    Object7.SetActive(false);
     
     // if(Object.Length >= 0){
     // (Object[Objects]).SetActive(false);
