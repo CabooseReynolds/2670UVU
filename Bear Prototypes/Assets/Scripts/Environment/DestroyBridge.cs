301b9ce824e2275206124ac50c98da037b5c4215
Unknown <caboosereynolds@gmail.com>
2017-10-26 10:58:42 -0600

    CHanges to bridge

--- Bear Prototypes/Assets/Scripts/Environment/DestroyBridge.cs
+++ Bear Prototypes/Assets/Scripts/Environment/DestroyBridge.cs
@@ -2,15 +2,37 @@
  using System.Collections;
  
  public class DestroyBridge : MonoBehaviour { 
-     public GameObject OBJ;
-     public float time;
-	public DestroyOnReset DBridge;
+    public GameObject OBJ;
+    public GameObject OBJ2;
+    public GameObject OBJ3;
+    public float time;
+    public float time2;
+    public float time3;
+    public GameObject bridge;
+
+void Start()
+{
+    Moveinput.Reset += Destroy;
+}
      void OnTriggerEnter(Collider other)
  {
      if (other.gameObject.CompareTag("Player"))
      {
-		  
-          DBridge.Destroy(); // if you want to destroy the key as well
+		  StartCoroutine(BridgeDestroy());
      }
  }
+
+ IEnumerator BridgeDestroy()
+ {
+     yield return new WaitForSeconds(0);
+     Destroy(OBJ, time);
+     Destroy(OBJ2, time2);
+     Destroy(OBJ3, time3);
+     StopAllCoroutines();
+ }
+public void Destroy()
+	{
+        Destroy(bridge);
+        Moveinput.Reset -= Destroy;
+    }
  }
