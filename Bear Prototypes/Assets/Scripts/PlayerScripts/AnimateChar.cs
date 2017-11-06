5629d23d3f469efc01fd07a2dd6437da59a7d863
Unknown <caboosereynolds@gmail.com>
2017-11-06 15:22:40 -0700

    level changes

--- Bear Prototypes/Assets/Scripts/PlayerScripts/AnimateChar.cs
+++ Bear Prototypes/Assets/Scripts/PlayerScripts/AnimateChar.cs
@@ -4,19 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimateChar : MonoBehaviour {
-
+CharacterController cc;
+[RequireComponent]
 Animator anims;
+Vector3 move;
+public float Speed = 5;
 	// Use this for initialization
 	void Start () {
+		cc = GetComponent<CharacterController>();
 		anims = gameObject.GetComponent<Animator>();
-		PlayButton.Play += OnPlay;
+	}
+void update (){
+	move.x = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+	anims.speed = move.x;
+	cc= 
 
-	}
-	void OnPlay () {
-				Moveinput.KeyAction += Animate;
-				anims.SetTrigger("IsLoaded");
-				PlayButton.Play -=OnPlay;
-	}
+}
 
     private void Animate(float obj)
     {
