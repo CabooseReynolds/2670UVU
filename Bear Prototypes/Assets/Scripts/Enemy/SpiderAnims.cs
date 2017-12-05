193d61e35e4d55335c82712d476468f604eb9bda
Unknown <caboosereynolds@gmail.com>
2017-12-05 12:37:16 -0700

    fixes to the game

---
+++ Bear Prototypes/Assets/Scripts/Enemy/SpiderAnims.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class SpiderAnims : MonoBehaviour 
+{
+	public static UnityAction PushAction;
+	Animator anims;
+	bool crouching = false;
+    
+//    NavMeshAgent
+
+// 	void Start () 
+// 	{
+// 		anims = GetComponentInChildren<Animator>();
+// 	}
+	
+// void Update()
+// {
+// anim.SetFloat("Walk", velocity.magnitude)
+
+// }
+}
