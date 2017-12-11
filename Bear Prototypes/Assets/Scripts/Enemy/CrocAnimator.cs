0ddca33b5fb71379affc4841a1c6753600599bae
Unknown <caboosereynolds@gmail.com>
2017-12-11 00:36:06 -0700

    Animations are not freaing working.. WTF!

---
+++ Bear Prototypes/Assets/Scripts/Enemy/CrocAnimator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrocAnimator : MonoBehaviour {
+
+	public Animator anims;
+	public GameObject Enemy;
+	void OnTriggerEnter(Collider other)
+	{
+		anims.SetTrigger("Swim");
+	}
+	void OnTriggerExit()
+	{
+		anims.SetTrigger("Swim");
+	}
+}
